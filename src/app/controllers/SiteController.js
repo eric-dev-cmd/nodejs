32acfb1a093189b7250c2702ece7bafdc3fecf83
zunggzing <hello.trungvinh@gmail.com>
2021-08-26 08:26:15 +0700

    Read from db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,21 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 class SiteController {
     /**
      * TODO: [GET] /
      */
-    index(req, res) {
-        Course.find({}, (err, courses) => {
-            if (err) return res.status(400).json({ err: 'Failure!' });
-            res.json(courses);
-        });
+    index(req, res, next) {
+        // Course.find({}, (err, courses) => {
+        //     if (err) return res.status(400).json({ err: 'Failure!' });
+        //     res.json(courses);
+        // });
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
     }
     /**
      * TODO: [GET] /search
